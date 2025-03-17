b86918185b5d79f2977a0df493c9d71e901c25e3
David Kizivat <david.kizivat@gmail.com>
2025-03-17 13:52:17 +0100

    zsh,yazi: open yazi with y; cd to dir on quit

--- zsh/.config/zsh/aliases.zsh
+++ zsh/.config/zsh/aliases.zsh
@@ -54,3 +54,12 @@ gh-clone() {
     gh repo clone "$repo" "$target_dir/$repo_name"
   done
 }
+
+function y() {
+	local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
+	yazi "$@" --cwd-file="$tmp"
+	if cwd="$(command cat -- "$tmp")" && [ -n "$cwd" ] && [ "$cwd" != "$PWD" ]; then
+		builtin cd -- "$cwd"
+	fi
+	rm -f -- "$tmp"
+}
